5c2dc3781fb4f8de8c92386312000bfa14a98a1f
Vassilis <bill-br@hotmail.com>
2019-11-06 00:38:08 +0200

    GUI validate

--- simplegui.py
+++ simplegui.py
@@ -5,59 +5,74 @@ import datetime
 import re
 from typing import Match
 
-import PySimpleGUI as sg
-import engine
+import PySimpleGUIQt as sg
 
 layout = [[sg.Text('START', size=(8, 1)), sg.Input('00:00', key='_start_', size=(5, 1), enable_events=True)],
-          [sg.Text('STOP', size=(8, 1)), sg.Input('00:00', key='_stop_', size=(5, 1))],
-          [sg.Text('Date', size=(8, 1)), sg.Input('01/01/2019', key='_date_', size=(10, 1))],
-          [sg.Text('Cycle ID', size=(8, 1)), sg.Input('1', key='_cycle_id_', size=(5, 1))],
-          [sg.Button('Submit', size=(8, 1)), sg.Exit(size=(5, 1))]]
+          [sg.Text('STOP', size=(8, 1)), sg.Input('00:00', key='_stop_', size=(5, 1), enable_events=True)],
+          [sg.Text('Date', size=(8, 1)), sg.Input('01/01/2019', key='_date_', size=(10, 1), enable_events=True)],
+          [sg.Text('Cycle ID', size=(8, 1)), sg.Input('1', key='_cycle_id_', size=(5, 1), enable_events=True)],
+          [sg.Button('Submit', size=(8, 1), key='_submit_', enable_events=True), sg.Exit(size=(5, 1))],
+          [sg.Text('test', size=(8, 1)), sg.Input('1', key='test', size=(5, 1), enable_events=True)]]
 
 window = sg.Window('engine data', layout)
-
-# layout[0][1].BackgroundColor = '#FF6666'
-# validate start, stop
-# match: Match = re.compile(r'\d\d:\d\d$').match(startInput)
-# valid = False
-# if match is not None:
-#     startInput.BackgroundColor = '#ffffff'
-#     print('match')
-#     valid = True
-# else:
-#     startInput.BackgroundColor = '#ff0000'
-#     print('no match')
-#     valid = False
-# messagebox = tk.Tk()
-# w = tk.Message(messagebox, text="this is a message")
-# w.pack()
-# start_time = datetime.datetime.strptime(start_time_string, "%H:%M").time()
-# start_entry.setvar('time', )
+bgColor_default = window['_start_'].TextColor
 
 # start: datetime.time = datetime.datetime.strptime(values[0], "%H:%M").time()
 # stop: datetime.time = datetime.datetime.strptime(values[1], "%H:%M").time()
 # date: datetime.date = datetime.datetime.strptime(values[2], "%d/%m/%Y").date()
 # cycle_id: int = int(values[3])
 
-time_regex = re.compile(r'\d\d:\d\d$')
-startInput = window['_start_']
+time_regex = re.compile(r'^(0\d|1\d|2[0-3]):[0-5]\d$')
+date_regex = re.compile(r'^(0\d|1\d|2\d|3[0-1])/(0\d|1[0-2])/(\d{4})$')
+int_regex = re.compile(r'\d+')
+
+
+def check_input(match: Match, element: str):
+    if match is None:
+        window[element].Update(background_color='#FF3333')
+        window[element].BackgroundColor = '#FF3333'
+    else:
+        window[element].Update(background_color=bgColor_default)
+        window[element].BackgroundColor = bgColor_default
+
+
 while True:
-    event, values = window.Read()
-    # print(window['_start_'])
-    # print(values)
+    event, values = window.Read(timeout=10)
+    window['_cycle_id_'].Update(background_color=bgColor_default)
+    # input check
     if event is '_start_':
-        print(startInput.BackgroundColor)
-        startInput.BackgroundColor = '#FF0000'
         match = time_regex.match(values['_start_'])
-        # if match is None:
-        #     window['_start_'].BackgroundColor = '#FF0000'
-        # else:
-        #     window['_start_'].BackgroundColor = '#FFFFFF'
-        # try:
-        #     start: datetime.time = datetime.datetime.strptime(values[0], "%H:%M").time()
-        # except ValueError or KeyError:
-        #     window['_start_'].BackgroundColor = '#FF0000'
-    if event in (None, 'Exit', 'Window closed using X'):
+        check_input(match, '_start_')
+    if event is '_stop_':
+        match = time_regex.match(values['_stop_'])
+        check_input(match, '_stop_')
+    if event is '_date_':
+        match = date_regex.match(values['_date_'])
+        check_input(match, '_date_')
+    if event is '_cycle_id_':
+        match = time_regex.match(values['_cycle_id_'])
+        check_input(match, '_cycle_id_')
+    # on submit click
+    if event is '_submit_':
+        error = ''
+        if window['_start_'].BackgroundColor != bgColor_default:
+            error += 'Wrong START input!\n'
+        elif window['_stop_'].BackgroundColor != bgColor_default:
+            error += 'Wrong STOP input!\n'
+        elif window['_date_'].BackgroundColor != bgColor_default:
+            error += 'Wrong date input!\n'
+        elif window['_cycle_id_'].BackgroundColor != bgColor_default:
+            error += 'Wrong cycle id input!\n'
+        if len(error) > 0:
+            sg.PopupAutoClose(error)
+        else:
+            start: datetime.time = datetime.datetime.strptime(values['_start_'], "%H:%M").time()
+            stop: datetime.time = datetime.datetime.strptime(values['_stop_'], "%H:%M").time()
+            date: datetime.date = datetime.datetime.strptime(values['_date_'], "%d/%m/%Y").date()
+            cycle_id: int = int(values['_cycle_id_'])
+            print(start)
+
+    if event in ('Exit', 'Window closed using X'):
         break
     # print(event, values)
     # print(values['_start_'])
