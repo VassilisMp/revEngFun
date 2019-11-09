c73c36b8250f8d0f8a3fff653db585bebc688045
Vassilis <bill-br@hotmail.com>
2019-11-09 15:15:45 +0200

    find const fields

--- parseData.py
+++ parseData.py
@@ -28,6 +28,31 @@ def getConstColNames(data) -> list:
     return const_cols
 
 
+def check_const_data_cols():
+    global i
+    const_cols = getConstColNames(engines[1].data_frame)
+    print('constant columns')
+    print(const_cols)
+    print('\n')
+    for i in range(1, len(engines)):
+        this_cols = getConstColNames(engines[i].data_frame)
+        if this_cols != const_cols:
+            print('{0}: {1}'.format(i, list(set(const_cols) ^ set(this_cols))))
+
+
+def check_const_info(name: str) -> bool:
+    global engine
+    info0 = set()
+    for engine in engines:
+        info0.add(engine.info[name])
+    # print(info0)
+    if len(info0) == 1:
+        return True
+    else:
+        # print('{0}: {1}'.format(name, info0))
+        return False
+
+
 # List all files in a dataFolder using os.listdir
 cur_path = os.getcwd()
 basepath = cur_path + '/dataFolder/'
@@ -36,6 +61,7 @@ for entry in os.listdir(basepath):
     if os.path.isfile(os.path.join(basepath, entry)):
         files.append(entry)
 
+# get engines
 engines: list = []
 for fileName in files:
     # read file lines
@@ -46,13 +72,13 @@ for fileName in files:
         lines[i] = lines[i].replace('\t', '')
     csv_reader = csv.reader(lines, delimiter=',')
     # get settings in list of dicts
-    settings: list = []
+    settings: dict = {}
     index = -1
     for row in csv_reader:
         index = index + 1
         if len(row) == 0:
             break
-        settings.append({row[0]: int(row[1])})
+        settings.update({row[0].strip(): int(row[1])})
     # move to next row, because this is empty
     csv_reader.__next__()
     # save data in csv to use parse dates with pandas
@@ -65,12 +91,21 @@ for fileName in files:
 # remove tmp file
 os.remove('tmp')
 
-const_cols = getConstColNames(engines[0].data_frame)
-print('constant columns')
-print(const_cols)
-print('\n')
+# find constant columns
+# check_const_data_cols()
 
-for i in range(1, len(engines)):
-    this_cols = getConstColNames(engines[i].data_frame)
-    if this_cols != const_cols:
-        print('{0}: {1}'.format(i, this_cols))
+# remove constant columns
+# 'Vessel Valve' is constant in some Data
+# Engine 18 Data has cycle stops
+for engine in engines:
+    engine.data_frame = engine.data_frame.drop(columns=['Cycle Number', 'Active Countdown', 'Overload Alarm', 'System '
+                                                                                                              'Alarm'])
+
+# check which info fields are constant
+var_info_names = []
+for field in list(engines[0].info.keys()):
+    if not check_const_info(field):
+        var_info_names.append(field)
+# info variable fields are 'Batch Number', 'Vessel Serial Number'
+
+
